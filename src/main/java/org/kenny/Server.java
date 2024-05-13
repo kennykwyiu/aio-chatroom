598acdcd3a4b4ad758d651cae58fb805d2e4264a
kennykwyiu <kenny.kwyiu@gmail.com>
2024-05-13 21:59:30 +0800

    13 May 2024 implemented start()

--- src/main/java/org/kenny/Server.java
+++ src/main/java/org/kenny/Server.java
@@ -2,11 +2,14 @@ package org.kenny;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
 
 public class Server {
 
     final String LOCALHOST = "localhost";
     final int DEFAULT_PORT = 8888;
+    AsynchronousServerSocketChannel serverChannel;
 
     private void close(Closeable closeable) {
         if (closeable != null) {
@@ -16,6 +19,24 @@ public class Server {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+        }
+    }
+
+    public void start() {
+        try {
+            // connect listener port
+            serverChannel = AsynchronousServerSocketChannel.open();
+            serverChannel.bind(new InetSocketAddress(LOCALHOST, DEFAULT_PORT));
+            System.out.println("Start server, listener port: " + DEFAULT_PORT);
+
+            while (true) {
+                serverChannel.accept(null, new AcceptHandler());
+                System.in.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(serverChannel);
         }
     }
 }
